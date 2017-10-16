a68db41dc389f01842923e48fa286435b3ec9f06
max <max@localhost>
2017-10-16 21:52:16 +0200

    Initial Service Worker (Commit Test)

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,17 @@
 /**
- * TODO
+ * TODO Service-Worker
  */
+var cacheName = "";
+var filesToCache = [];
+
+self.addEventListener('install', function(e) {
+	console.log('[ServiceWorker] Install');
+});
+
+self.addEventListener('activate', function(e) {
+	console.log('[ServiceWorker] Activate');
+});
+
+self.addEventListener('fetch', function(e) {
+	console.log('[Service Worker] Fetch', e.request.url);
+});
