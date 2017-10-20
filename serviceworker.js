a4ee040c8a9b21bb3fa80838a08dc1e589f69753
max <max@localhost>
2017-10-20 20:30:30 +0200

    Initial Service Worker (Commit Test)

--- serviceworker.js
+++ serviceworker.js
@@ -4,14 +4,30 @@
 var cacheName = "";
 var filesToCache = [];
 
+/**
+ * Beim ersten Aufruf der Seite wird 'install' aufgerufen
+ * @param e
+ * @returns
+ */
 self.addEventListener('install', function(e) {
 	console.log('[ServiceWorker] Install');
 });
 
+/**
+ * Nach der Installation wird Service-Worker aktiviert
+ * @param e
+ * @returns
+ */
 self.addEventListener('activate', function(e) {
 	console.log('[ServiceWorker] Activate');
 });
 
+
+/**
+ * Bei jedem Netzwerkzugriff (reload, request) wird gefetcht
+ * @param e
+ * @returns
+ */
 self.addEventListener('fetch', function(e) {
 	console.log('[Service Worker] Fetch', e.request.url);
 });
